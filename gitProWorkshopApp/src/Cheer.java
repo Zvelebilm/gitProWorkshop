5ac9e48f0e8deae526b4b6537017d28debf28fa2
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2023-12-04 13:12:17 +0100

    c6

--- gitProWorkshopApp/src/Cheer.java
+++ gitProWorkshopApp/src/Cheer.java
@@ -1,12 +1,12 @@
 public class Cheer {
 
-<<<<<<< HEAD
+
     public Cheer(){
         System.out.println("Java");
-=======
+
 
     public Cheer(String string){
         System.out.println(string);
->>>>>>> 3023fa98772fade998a9922062815c33592d006d
+
     }
 }
--- gitProWorkshopApp/src/MyClass.java
+++ gitProWorkshopApp/src/MyClass.java
@@ -1,10 +1,10 @@
 public class MyClass {
-<<<<<<< HEAD
+
     public  void greet(){
         System.out.println("Greetings greetings");
-=======
+
     public  void greet(String text){
         System.out.println("Marek Zvelebil"+text);
->>>>>>> be499376379e5c337a45493babd89bfacdb7aa8b
+
     }
 }
