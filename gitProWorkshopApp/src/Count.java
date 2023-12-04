684a7239b75fd9204e7a94646caa94cb77dd8c23
Davias97 <da.vesely@email.cz>
2023-12-04 13:52:44 +0100

    b5

--- gitProWorkshopApp/src/Count.java
+++ gitProWorkshopApp/src/Count.java
@@ -1,10 +1,11 @@
 public class Count {
 
     private int age = 26;
+    private String sentence = " years old.";
 
     public void count() {
 
-        System.out.println(age + " years old.");
+        System.out.println(age + sentence);
 
         System.out.println("5");
 
